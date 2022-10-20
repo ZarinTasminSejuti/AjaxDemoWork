f2722631fa3db9b901272743d96a6023cc2ffa96
Zarin Tasmin Sejuti <zarintasmin@Zarins-MacBook-Air.local>
2022-10-20 13:12:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function loadData(){
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function(){
+        const container = document.getElementById("demo");
+        container.innerHTML = xhr.responseText;
+    }
+
+    xhr.open("GET", "./data/data.txt");
+
+    xhr.send();
+
+}
